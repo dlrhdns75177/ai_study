7e0ee71568cbc9da5c2aa35dd02afdd3538d0fe6
dlrhdns75177 <sararhdns7517@naver.com>
2024-12-02 21:47:41 +0900

    주석 수정

--- 1week_code/숫자 맟추기.py
+++ 1week_code/숫자 맟추기.py
@@ -7,14 +7,17 @@ def number_game(): #함수 정의
 
     while True: #무한 반복
         number_pre = int(input("예상 숫자 : ")) #사용자가 예상 숫자를 입력하고 그 수를 변수에 저장
-
-        if number_pre == number: #사용자가 예상한 숫자와 랜덤으로 뽑은 수가 같다면 
-            print("정답입니다!") #출력
-            break #반복문을 나온다
-        elif number_pre < number: #사용자가 예상한 숫자가 랜덤으로 뽑은 수보다 작다면
-            print("숫자가 작습니다. 다시 입력하세요.") #출력하고 다시 입력하도록 한다.
-        else: #사용자가 예상한 숫자가 랜덤으로 뽑은 수보다 크다면
-            print("숫자가 큽니다. 다시 입력하세요") #출력하고 다시 입력하도록 한다.
+        if number_pre < 11 and number_pre > 0: #만약에 사용자가 입력한 숫자가 11보다 작고 0보다 크다면
+            if number_pre == number: #사용자가 예상한 숫자와 랜덤으로 뽑은 수가 같다면 
+                print("정답입니다!") #출력
+                break #반복문을 나온다
+            elif number_pre < number: #사용자가 예상한 숫자가 랜덤으로 뽑은 수보다 작다면
+                print("숫자가 작습니다. 다시 입력하세요.") #출력하고 다시 입력하도록 한다.
+            else: #사용자가 예상한 숫자가 랜덤으로 뽑은 수보다 크다면
+                print("숫자가 큽니다. 다시 입력하세요") #출력하고 다시 입력하도록 한다.
+        else: #그렇지 않으면 
+            print('1부터 10까지의 숫자만 입력해주세요.') #출력
+        
 
 def game_restart(): #게임 재시작 여부 함수 정의
     while True: #break만나기 전까지 무한 반복
